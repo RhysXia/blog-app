f61c7651407a2f031b4bca2b1ef0da557971638e
Ryths Xia <xrs4433@outlook.com>
2017-10-22 23:03:13 +0800

    为文章点赞评论添加判断逻辑

--- app/src/main/kotlin/cn/ryths/blog/app/view/fragment/ArticleFragment.kt
+++ app/src/main/kotlin/cn/ryths/blog/app/view/fragment/ArticleFragment.kt
@@ -2,16 +2,19 @@ package cn.ryths.blog.app.view.fragment
 
 import android.app.Fragment
 import android.databinding.DataBindingUtil
+import android.databinding.ObservableField
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.webkit.JavascriptInterface
+import android.widget.Toast
 import cn.ryths.blog.app.R
 import cn.ryths.blog.app.databinding.FragmentArticleBinding
 import cn.ryths.blog.app.entity.Article
 import cn.ryths.blog.app.service.ArticleService
 import cn.ryths.blog.app.service.ServiceCallback
+import cn.ryths.blog.app.view.viewModel.GlobalViewModel
 
 class ArticleFragment : Fragment() {
 
@@ -36,10 +39,12 @@ class ArticleFragment : Fragment() {
             activity.finish()
         }
         binding.fragmentArticleContent.settings.javaScriptEnabled = true
-
+        binding.globalViewModel = GlobalViewModel.getInstance()
+        val viewModel = ViewModel()
+        binding.viewModel = viewModel
         articleService.findById(articleId, object : ServiceCallback<Article?, Void?> {
             override fun success(result: Article?) {
-                binding.article = result
+                viewModel.article.set(result)
                 binding.fragmentArticleContent.addJavascriptInterface(JsClient(result!!.content), "JsClient")
                 binding.fragmentArticleContent.loadUrl("file:///android_asset/markdown.html")
             }
@@ -49,7 +54,6 @@ class ArticleFragment : Fragment() {
             }
 
         })
-
         return binding.root
     }
 
@@ -62,5 +66,24 @@ class ArticleFragment : Fragment() {
          */
         @JavascriptInterface
         fun getContent() = content
+    }
+
+    inner class ViewModel {
+        var article: ObservableField<Article> = ObservableField()
+
+        fun praiseClick() {
+            val isLogin = GlobalViewModel.getInstance().login
+            if (!isLogin) {
+                Toast.makeText(this@ArticleFragment.activity, "请登录后再进行点赞", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        fun commentClick() {
+            val isLogin = GlobalViewModel.getInstance().login
+            if (!isLogin) {
+                Toast.makeText(this@ArticleFragment.activity, "请登录后再进行评论", Toast.LENGTH_SHORT).show()
+            }
+
+        }
     }
 }
